c3be1bb589baac1c90efecbc20b301a57b7d365b
soumyadip099 <74173455+soumyadip099@users.noreply.github.com>
2021-01-13 21:35:09 +0530

    Add files via upload

---
+++ contact-form.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, NgForm, FormControl} from '@angular/forms';
+import { CserviceService} from '../cservice.service';
+import { Model} from '../model';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent implements OnInit {
+
+  model : Model = {
+    "fullname": "aaaaa",
+    "email": "jjh@sdf.nbh",
+    "comment": "jhfhgdgdgfd"
+  }
+
+  FormData: FormGroup;
+  constructor(private builder: FormBuilder, private contact: CserviceService) { }
+
+  ngOnInit() {
+    this.FormData=this.builder.group({
+        Fullname: new FormControl('', [Validators.required]),
+        Email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
+        comment: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit(FormData){
+    console.log(FormData)
+    this.contact.PostMessage(FormData).subscribe(Response=>{
+      location.href='https://mailthis.to/confirm'
+      console.log(Response)
+    },error=>{
+      console.warn(error.responseText)
+      console.log({error})
+    })
+  }
+
+}
